542db61e9c19d342e68e96084d77d89cf8a8eced
slwu89 <10673535+slwu89@users.noreply.github.com>
2022-05-12 10:58:14 -0700

    sample input data

--- model.R
+++ model.R
@@ -3,6 +3,62 @@ library(jsonlite)
 library(jsonvalidate)
 library(rstudioapi)
 
+# make parameters for new schema
+p <- 20
+tmax <- 1095
+
+surv_a <- 0.9
+
+adult_stochastic <- TRUE
+adult_f <- 0.3
+adult_q <- 0.9
+adult_eip <- rep(10, tmax)
+adult_p <- t(replicate(n = p, expr = {(sin((1:tmax)/365*2*pi) + 2)/2 * surv_a}))
+adult_psi <- diag(p)
+adult_nu <- 25
+adult_M <- rpois(n = p, lambda = 100)
+adult_Y <- rep(0, times = p)
+adult_Z <- rep(0, times = p)
+
+molt <- 0.1
+surv <- 0.9
+
+aqua_stochastic <- TRUE
+aqua_molt <- matrix(data = molt, nrow = p, ncol = tmax)
+aqua_surv <- matrix(data = surv, nrow = p, ncol = tmax)
+
+lambda <- adult_M*(1-surv_a)
+eggs <- adult_nu * adult_f * adult_M
+
+aqua_L <- lambda * ((1/molt) - 1) + eggs
+aqua_K <- - (lambda * aqua_L) / (lambda - aqua_L*molt*surv)
+aqua_K <- t(sapply(X = aqua_K, FUN = function(x) {rep(x, tmax)}))
+
+input <- list(
+  p = p,
+  tmax = tmax,
+  aqua_stochastic = aqua_stochastic,
+  aqua_molt = aqua_molt,
+  aqua_surv = aqua_surv,
+  aqua_K = aqua_K,
+  aqua_L = aqua_L,
+  adult_stochastic = adult_stochastic,
+  adult_f = adult_f,
+  adult_q = adult_q,
+  adult_eip = adult_eip,
+  adult_p = adult_p,
+  adult_psi = adult_psi,
+  adult_nu = adult_nu,
+  adult_M = adult_M,
+  adult_Y = adult_Y,
+  adult_Z = adult_Z
+)
+
+jsonlite::write_json(x = input, path = "input/input.json",digits=17,pretty=TRUE)
+
+jsonvalidate::json_validate(json = "input/input.json", schema = "schema/input-MicroMoB.json")
+
+# old ----------------------------------------------------------------------
 ARGS <- commandArgs(trailingOnly=TRUE)
 print(ARGS)
 # IO
